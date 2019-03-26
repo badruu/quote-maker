24316b891a4c07d7a4340fb5bb9a5875d7d13cf5
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-03-26 17:05:26 +0300

    add new quotes, add background colors for the cards

--- src/app/quote-view/quote-view.component.ts
+++ src/app/quote-view/quote-view.component.ts
@@ -9,8 +9,9 @@ import {Quote } from '../quote'
 export class QuoteViewComponent implements OnInit {
 
   quotes = [
-    new Quote('Anonymous', 'Wherever life plants you, bloom with grace', 'Badrudin', new Date (2019,3,27), 0, 0),
-    new Quote('Anonymous', 'Wherever life plants you, bloom with grace', 'Badrudin', new Date (2019,3,27), 0, 0)
+    new Quote('Chinese', 'The best thing is to be respected, the next is to be loved; it is bad to be hated, but still worse to be despised.', 'Badrudin', new Date (2019,3,27), 0, 0),
+    new Quote('Hitopadesa', 'Circumspection in calamity; mercy in greatness; good speeches in assemblies; fortitude in adversity: these are the self-attained perfections of great souls.', 'Badrudin', new Date (2019,3,27), 0, 0),
+    new Quote('Magha', 'The intellect of a good man is piercing, yet inflicts no wound; his actions are deliberate, yet bold; his heart is warm, but never burns; his speech is eloquent, yet very true.', 'Badrudin', new Date (2019,3,27), 0, 0)
   ]
 
   addNewQuote(quote){
